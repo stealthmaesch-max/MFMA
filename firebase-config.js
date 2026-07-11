38f0606cb36243a719618392138f734d31eece5f
Stealth M <stealthmaesch@icloud.com>
2026-07-11 12:56:43 -0500

    Delete firebase-config.js

--- firebase-config.js
+++
@@ -1,9 +0,0 @@
-export const firebaseConfig = {
-  apiKey: "AIzaSyB508WoQS1RMQdz4KHuclvRDIxg4gFnQSU",
-  authDomain: "mfma-flag-system.firebaseapp.com",
-  databaseURL: "https://mfma-flag-system-default-rtdb.firebaseio.com",
-  projectId: "mfma-flag-system",
-  storageBucket: "mfma-flag-system.firebasestorage.app",
-  messagingSenderId: "396757625279",
-  appId: "1:396757625279:web:6e22be1286702ff463990a"
-};
