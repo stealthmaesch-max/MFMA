680781862efc252bdc4a377a61ec224144da8102
Stealth M <stealthmaesch@icloud.com>
2026-07-11 12:56:24 -0500

    Delete personnel.js

--- personnel.js
+++
@@ -1,8 +0,0 @@
-export const personnel = {
-  marilyn:{name:"Marilyn Maeschen",canDrive:true,vehicles:["ranger","shelly","gator"]},
-  stealth:{name:"Stealth Maeschen",canDrive:true,vehicles:["ranger","shelly","gator"]},
-  lexis:{name:"Lexis Maeschen",canDrive:true,vehicles:["shelly","gator"]},
-  alektra:{name:"Alektra Maeschen",canDrive:true,vehicles:["shelly","gator"]},
-  rockwell:{name:"Rockwell Maeschen",canDrive:false,vehicles:[]}
-};
-export const vehicles={ranger:{name:"Ranger"},shelly:{name:"Shelly"},gator:{name:"Gator"}};
